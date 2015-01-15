1a84d88ce8a3d091f0ebe34422665735c0f8a94e
WadaHayato <hatoya.web.design@gmail.com>
2015-01-15 19:07:01 +0900

    add angular repeat

--- ts/app.ts
+++ ts/app.ts
@@ -2,4 +2,12 @@ declare var angular: any;
 
 module hatoya {
     var app = angular.module('hatoyaApp', []);
+    var content = {
+        'profile': ['Name: Wada Hayato', 'Birth: 1993/9/16', 'Age: 20', 'School: Nihon Uni.'],
+        'skills': ['HTML', 'CSS', 'javaScript', 'jQuery', 'AngularJS','Sass', 'Grunt', 'Gulp', 'PHP', 'CakePHP', 'C, C++', 'Processing', 'openFrameworks','Illustrator', 'Photoshop']
+    };
+
+    app.controller('contentController', function(){
+        this.data = content;
+    });
 }
